3d2aaa098a7b53c233ba0826117eddaa53d2e4c3
s4ymyn4mee <vadim.ganiev25@mail.ru>
2022-10-03 17:06:27 +0500

    [HW-1-3] Сделал HW-1-3 :) :) :)

---
+++ HW-1-3/HW-1-3.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    float N, A, B, X, Y;
+    cin >> N >> A >> B >> X >> Y;
+    if (N>B) cout << N - N*Y/100;
+    else if (N>A) cout << N - N*X/100;
+    else cout << N;
+}

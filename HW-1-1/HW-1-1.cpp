65b28254af0947db48f37862c5be5a3432ab6403
s4ymyn4mee <vadim.ganiev25@mail.ru>
2022-10-08 13:03:10 +0500

    [HW-1-1] Поправил опять HW-1-1

--- HW-1-1/HW-1-1.cpp
+++ HW-1-1/HW-1-1.cpp
@@ -6,17 +6,19 @@ using namespace std;
 int main() {
     float xPointLineFirst, xPointLineSecond, yLineFirst, yLineSecond, xPoint, yPoint;
     cin >> xPointLineFirst >> yLineFirst >> xPointLineSecond >> yLineSecond >> xPoint >> yPoint;
-    float distance = (abs((yLineSecond - yLineFirst) * xPoint - (xPointLineSecond - xPointLineFirst) * yPoint \
-    + yLineFirst * xPointLineSecond - xPointLineFirst * yLineSecond)) / (sqrt((yLineSecond - yLineFirst) \
-    * (yLineSecond - yLineFirst) + (xPointLineSecond - xPointLineFirst) * (xPointLineSecond - xPointLineFirst)));
-    float xCrossing = ((xPointLineFirst - xPointLineSecond) * (xPointLineFirst - xPointLineSecond) * xPoint \
-    + (xPointLineSecond - xPointLineFirst) * (yLineSecond - yLineFirst) * yPoint - (yLineFirst * xPointLineSecond \
-    - xPointLineFirst * yLineSecond) * (yLineSecond - yLineFirst)) / ((yLineSecond - yLineFirst) * (yLineSecond \
-    - yLineFirst) + (xPointLineSecond - xPointLineFirst) * (xPointLineSecond - xPointLineFirst));
+    float distance = (abs((yLineSecond - yLineFirst) * xPoint - (xPointLineSecond - xPointLineFirst) * yPoint +
+                          yLineFirst * xPointLineSecond - xPointLineFirst * yLineSecond)) /
+                     (sqrt((yLineSecond - yLineFirst) * (yLineSecond - yLineFirst) +
+                           (xPointLineSecond - xPointLineFirst) * (xPointLineSecond - xPointLineFirst)));
+    float xCrossing = ((xPointLineFirst - xPointLineSecond) * (xPointLineFirst - xPointLineSecond) * xPoint +
+                       (xPointLineSecond - xPointLineFirst) * (yLineSecond - yLineFirst) * yPoint -
+                       (yLineFirst * xPointLineSecond - xPointLineFirst * yLineSecond) * (yLineSecond - yLineFirst)) /
+                      ((yLineSecond - yLineFirst) * (yLineSecond - yLineFirst) +
+                       (xPointLineSecond - xPointLineFirst) * (xPointLineSecond - xPointLineFirst));
     float yCrossing;
     if (yLineSecond != yLineFirst)
-        yCrossing = ((xPointLineFirst - xPointLineSecond) * xCrossing + (yLineSecond - yLineFirst) * yPoint \
-        + (xPointLineSecond - xPointLineFirst) * xPoint) / (yLineSecond - yLineFirst);
+        yCrossing = ((xPointLineFirst - xPointLineSecond) * xCrossing + (yLineSecond - yLineFirst) * yPoint +
+                     (xPointLineSecond - xPointLineFirst) * xPoint) / (yLineSecond - yLineFirst);
     else
         yCrossing = yLineFirst;
     cout << distance << " " << xCrossing << " " << yCrossing;

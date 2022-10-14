650d05b46c57c8829f01e34fb2b62a79e3eedd18
s4ymyn4mee <vadim.ganiev25@mail.ru>
2022-10-15 01:23:06 +0500

    [HW-2-2] Поправил HW-2-2

---
+++ HW-2-2/HW-2-2.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(){
+    string stringLine;
+    getline(cin, stringLine);
+    int fCounter = 0;
+    for (int i = 0; i < stringLine.length(); i ++){
+        if (stringLine[i] == 'f'){
+            fCounter ++;
+            if (fCounter == 2)
+                cout << i;
+        }
+    }
+    if (fCounter == 1)
+        cout << -1;
+    if (fCounter == 0)
+        cout << -2;
+}

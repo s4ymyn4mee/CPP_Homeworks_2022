09190e49f89ee5abc9fe2a7ccedb3ebd97102b8d
s4ymyn4mee <vadim.ganiev25@mail.ru>
2022-10-20 22:40:02 +0500

    [HW-3-2] Ладно, исправил HW-3-2

--- HW-3-2/HW-3-2.cpp
+++ HW-3-2/HW-3-2.cpp
@@ -1,12 +1,17 @@
 #include <iostream>
-#include <cmath>
 
 using namespace std;
 
 int main(){
     int hourPointer, minutePointer;
     cin >> hourPointer >> minutePointer;
-    cout << (hourPointer + int(pow(-1, hourPointer >= 6)) * 2 * abs(hourPointer - 6)) % 12 << " ";
-    cout << (minutePointer + int(pow(-1, minutePointer >= 30)) * 2 * abs(minutePointer - 30)) % 60;
+    if (hourPointer >= 6)
+        cout << (hourPointer - 2 * abs(hourPointer - 6)) % 12 << " ";
+    else
+        cout << (hourPointer + 2 * abs(hourPointer - 6)) % 12 << " ";
+    if (minutePointer >= 30)
+        cout << (minutePointer - 2 * abs(minutePointer - 30)) % 60 << " ";
+    else
+        cout << (minutePointer + 2 * abs(minutePointer - 30)) % 60 << " ";
     return 0;
 }

da2d604fb766d789894f20f68372b6aa387a6645
s4ymyn4mee <vadim.ganiev25@mail.ru>
2022-10-23 16:39:26 +0500

    [HW-3-2] Поправил еще и еще раз HW-3-2

--- HW-3-2/HW-3-2.cpp
+++ HW-3-2/HW-3-2.cpp
@@ -5,7 +5,6 @@ using namespace std;
 int main(){
     int hourPointer, minutePointer;
     cin >> hourPointer >> minutePointer;
-    cout << (hourPointer - 2 * (hourPointer - 6)) % 12 << " ";
-    cout << (minutePointer - 2 * (minutePointer - 30)) % 60;
+    cout << (12 - hourPointer) % 12 << " " << (60 - minutePointer) % 60;
     return 0;
 }

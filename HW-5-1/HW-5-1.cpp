de2cab02bee59a90ce1ea57ca6d8f0950c8ed051
s4ymyn4mee <vadim.ganiev25@mail.ru>
2022-11-02 23:58:50 +0500

    [HW-5-1] Сделал hw-5-1

---
+++ HW-5-1/HW-5-1.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+string clearString(string line) {
+    string newLine = "";
+    int countOfAts = 0;
+    int i = 0;
+    while (i < line.length()) {
+        if (line[i] != '@')
+            newLine += line[i++];
+        else {
+            countOfAts = 0;
+            for (int j = i; j < line.length(); j++) {
+                if (line[j] == '@') {
+                    countOfAts++;
+                    newLine.erase(newLine.size() - 1);
+                } else
+                    break;
+            }
+            i += countOfAts;
+        }
+    }
+    return newLine;
+}
+
+int main() {
+    string line;
+    getline(cin, line);
+    cout << clearString(line) << endl;
+    return 0;
+}

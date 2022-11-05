b8b6370a7d61615cf86c6a5cb5f860eefef327c0
s4ymyn4mee <vadim.ganiev25@mail.ru>
2022-11-05 20:30:42 +0500

    [HW-4-2] Сделал hw-4-2 xD

--- HW-4-2/HW-4-2.cpp
+++ HW-4-2/HW-4-2.cpp
@@ -4,18 +4,20 @@
 using namespace std;
 
 int main() {
-    string word, palindromeLengthTwo, palindromeLengthThree;
+    string word;
     getline(cin, word);
     string minimalPalindromeLengthTwo = "~~~~";
     string minimalPalindromeLengthThree = "~~~~";
     for (int i = 0; i < word.length(); i++) {
         if (word[i] == word[i + 1]) {
-            palindromeLengthTwo = word[i];
+            string palindromeLengthTwo = "";
+            palindromeLengthTwo += word[i];
             palindromeLengthTwo += word[i + 1];
             if (palindromeLengthTwo < minimalPalindromeLengthTwo)
                 minimalPalindromeLengthTwo = palindromeLengthTwo;
         }
         if (word[i] == word[i + 2]) {
+            string palindromeLengthThree = "";
             palindromeLengthThree = word[i];
             palindromeLengthThree += word[i + 1];
             palindromeLengthThree += word[i + 2];

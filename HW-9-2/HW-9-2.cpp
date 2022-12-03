9a16096bd8efbee91ff25a1c1ff896dddb2d74e8
s4ymyn4mee <vadim.ganiev25@mail.ru>
2022-12-03 21:58:31 +0500

    [HW-9-2] Поправил 9-2 (точно без ошибок!)

--- HW-9-2/HW-9-2.cpp
+++ HW-9-2/HW-9-2.cpp
@@ -2,18 +2,23 @@
 
 using namespace std;
 
+void sumMatrices(int firstArray[5][5], int secondArray[5][5]) {
+    for (int i = 0; i < 5; i++) {
+        for (int j = 0; j < 5; j++)
+            cout << firstArray[i][j] + secondArray[i][j] << " ";
+        cout << endl;
+    }
+}
+
 int main() {
-    int firstArray[5][5], secondArray[5][5];
+    const int lengthArray = 5;
+    int firstArray[lengthArray][lengthArray], secondArray[lengthArray][lengthArray];
     for (int i = 0; i < 5; i++)
         for (int j = 0; j < 5; j++)
             cin >> firstArray[i][j];
     for (int i = 0; i < 5; i++)
         for (int j = 0; j < 5; j++)
             cin >> secondArray[i][j];
-    for (int i = 0; i < 5; i++) {
-        for (int j = 0; j < 5; j++)
-            cout << firstArray[i][j] + secondArray[i][j] << " ";
-        cout << endl;
-    }
+    sumMatrices(firstArray, secondArray);
     return 0;
 }

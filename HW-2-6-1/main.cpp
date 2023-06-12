1f699b00c6c37f1f0b3e2b2ecb09124fddc56bd2
s4ymyn4mee <vadim.ganiev25@mail.ru>
2023-06-12 17:29:54 +0500

    [HW-2-6-1] Сделал 2-6-1

--- HW-2-6-1/main.cpp
+++ HW-2-6-1/main.cpp
@@ -1,7 +1,7 @@
 #include "Graph.cpp"
 
 int main() {
-    UndirectedGraph graph(5);
+    UndirectedGraph graph(6);
     graph.AddEdge(0, 1);
     graph.AddEdge(0, 2);
     graph.AddEdge(2, 3);
@@ -9,7 +9,8 @@ int main() {
     graph.AddEdge(1, 4);
     graph.AddEdge(2, 4);
     graph.AddEdge(3, 4);
-    graph.Print();
-    std::cout << graph.BfsShortestPath(graph, 0, 4);
+    graph.AddEdge(4, 5);
+    //graph.Print();
+    std::cout << graph.BfsShortestPath(graph, 0, 5);
     return 0;
 }
